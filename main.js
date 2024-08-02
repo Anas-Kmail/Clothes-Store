5dec7fab906b344ec45c1f837b133657bd688e32
Anas-Kmail <145199331+Anas-Kmail@users.noreply.github.com>
2024-08-02 19:01:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let Menuitem=document.getElementById("Menu-items");
+Menuitem.style.maxheight="0px";
+function menutoggle(){
+if(Menuitem.style.maxheight=="0px")
+    Menuitem.style.maxheight="200px";
+else
+Menuitem.style.maxheight="0px";
+
+}
+console.log("hi");
